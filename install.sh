92f15342a1f3b2412edd1d19df72566310ca8f88
Priit Haamer <priit@haamer.ee>
2020-02-29 18:59:43 +0200

    feat: link dotfiles

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ dotfiles_directory=$HOME/.dotfiles
 
 warnmsg="\\033[33mWARN:\\033[0m"
 infomsg="\\033[36mINFO:\\033[0m"
+checkmark="\\033[32m✓\\033[0m"
+clearline="\\033[2K"
 
 install_homebrew() {
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -24,3 +26,22 @@ if [ ! -e $dotfiles_directory ]; then
   printf "${infomsg} Done.\\n"
 fi
 
+link_dotfiles() {
+  printf "${clearline}${infomsg} Linking dotfiles...\\n"
+
+  for source in $dotfiles_directory/dotfiles/*
+  do
+    target="$HOME/.`basename $source`"
+
+    if [ -f $target ] || [ -d $target ]
+    then
+      printf "${checkmark} File exists: ${target}\\n"
+    else
+      printf "${checkmark} Linking: ${source} -> ${target}\\n"
+      ln -sh $source $target
+    fi
+  done
+
+}
+
+link_dotfiles
