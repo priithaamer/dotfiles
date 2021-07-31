769af071cdd43d9e7bc0658115390c794e813689
Priit Haamer <priit@haamer.ee>
2021-07-31 20:31:15 +0300

    refactor: use /opt/homebrew in fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,13 @@
 set -x PATH /opt/homebrew/bin $PATH ./node_modules/.bin /usr/local/opt/openjdk/bin
 
 # I have it here so that node-rdkafka for will build
-set -x CPPFLAGS -I/usr/local/opt/openssl/include
-set -x LDFLAGS -L/usr/local/opt/openssl/lib
+set -x CPPFLAGS -I/opt/homebrew/opt/openssl/include
+set -x LDFLAGS -L/opt/homebrew/opt/openssl/lib
 
 if test -e ~/.local/share/fish/exports.fish
   source ~/.local/share/fish/exports.fish
+end
+
+if test -e /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+  source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 end
