248a46f32c04eb70b7d114658336705978fff7d5
Priit Haamer <priit@haamer.ee>
2022-10-06 16:10:29 +0300

    chore: remove openjdk from path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -x PATH /opt/homebrew/bin $PATH ./node_modules/.bin /usr/local/opt/openjdk/bin
+set -x PATH /opt/homebrew/bin $PATH ./node_modules/.bin
 
 # I have it here so that node-rdkafka for will build
 set -x CPPFLAGS -I/opt/homebrew/opt/openssl/include
