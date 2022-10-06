7f4bc5819a166d9a74e4367e41d2abf4a76b5715
Priit Haamer <priit@haamer.ee>
2022-10-06 16:11:06 +0300

    feat: alias ls to exa

--- fish/config.fish
+++ fish/config.fish
@@ -22,3 +22,5 @@ end
 if test -e /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
   source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 end
+
+alias ls='exa -al --color=always'
