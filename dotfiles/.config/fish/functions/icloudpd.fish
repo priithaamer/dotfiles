879233d3176edab1235b7ff9f2f4abafa64fac98
Priit Haamer <priit@haamer.ee>
2025-11-10 18:17:20 +0200

    chore: icloudpd paths

--- dotfiles/.config/fish/functions/icloudpd.fish
+++ dotfiles/.config/fish/functions/icloudpd.fish
@@ -1,3 +1,3 @@
 function icloudpd
-  docker run -it --rm --name icloudpd -v /Volumes/Photos/Photos:/data -v /Users/priit/.pyicloud:/root/.pyicloud icloudpd/icloudpd:latest icloudpd --directory /data --username priit@haamer.ee $argv
+  docker run -it --rm --name icloudpd -v /Volumes/Box/Photos/Backup:/data -v /Users/priit/.pyicloud:/root/.pyicloud icloudpd/icloudpd:latest icloudpd --directory /data --username priit@haamer.ee $argv
 end
