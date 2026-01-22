8baa4a1f6fc2b2e2b97be005a0cb1e0f54a55798
Priit Haamer <priit@haamer.ee>
2026-01-22 10:04:22 +0200

    chore: clean up fish config

--- dotfiles/.config/fish/config.fish
+++ dotfiles/.config/fish/config.fish
@@ -1,22 +1,29 @@
 set -x GOPATH $HOME/Developer/go
-set -x MODULAR_HOME $HOME/.modular
-set -x PATH /opt/homebrew/bin /Users/priit/.cargo/bin $MODULAR_HOME/pkg/packages.modular.com_mojo/bin $GOPATH/bin $PATH ./node_modules/.bin /opt/homebrew/opt/node@18/bin
-# fish_add_path /opt/homebrew/bin
-# fish_add_path /Users/priit/.cargo/bin
-# fish_add_path ./node_modules/.bin
-# fish_add_path /opt/homebrew/opt/node@18/bin
+set -x PATH $PATH ./node_modules/.bin
+
+fish_add_path /opt/homebrew/bin
+fish_add_path /Users/priit/.cargo/bin
+fish_add_path $GOPATH/bin
+fish_add_path $HOME/.local/bin
 
 set fish_greeting
-
-# I have it here so that node-rdkafka for will build
-set -x CPPFLAGS -I/opt/homebrew/opt/openssl/include
-set -x LDFLAGS -L/opt/homebrew/opt/openssl/lib
-set -gx LDFLAGS "-L/opt/homebrew/opt/node@18/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/node@18/include"
 
 set -g fish_color_autosuggestion "yellow"
 
 set -x USE_GKE_GCLOUD_AUTH_PLUGIN "True"
+
+# Git aliases
+alias gs="git status"
+alias gp="git pull"
+alias gpu="git push"
+alias ga="git add"
+alias gco="git checkout"
+alias gcm="git commit"
+
+# Yarn aliases
+alias y="yarn"
+alias yt="yarn test"
+alias yb="yarn build"
 
 if test -e ~/.local/share/fish/exports.fish
   source ~/.local/share/fish/exports.fish
@@ -24,8 +31,14 @@ end
 
 if test -e /opt/homebrew/bin/zoxide
   zoxide init fish | source
+  # "j" is better for zoxide as it stands for jumping
+  alias j=z
 end
 
 if test -e /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
   source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
